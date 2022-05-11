aade7447083349a6cafb20bf0b32413c440c444f
Jessica Santiago <jessie_13084@yahoo.com>
2022-05-10 20:09:53 -0400

    commiting what I have for this repo

--- index.js
+++ index.js
@@ -1,26 +1,159 @@
+// Include packages
+const fs = require("fs");
+const inquirer = require("inquirer");
+const { generateHTML } = require("./src/generateHTML");
+const path = require("path");
+const Employee = require("./lib/Employee");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const Manager = require("./lib/Manager");
+const teamList = [];
+
+// function addMembers() {
+//     // what object properties should be included here?
+// }
+
+// _______________-------create questions arrays--------___________________________
+
+// Manager Questions Array
+const managerQuestions = [
+  {
+    type: "input",
+    name: "name",
+    message: "What is the new Team Manager's name?",
+  },
+  {
+    type: "input",
+    name: "id",
+    message: "What is the new Team Manager's employee ID number?",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is the new Team Manager's email?",
+  },
+  {
+    type: "input",
+    name: "officeNumber",
+    message: "What is the new Team Manager's Office Number?",
+  },
+];
+// Engineer questions array
+const engineerQuestions = [
+  {
+    type: "input",
+    name: "name",
+    message: "What is the new Team Engineer's name?",
+  },
+  {
+    type: "input",
+    name: "id",
+    message: "What is the new Team Engineer's employee ID number?",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is the new Team Engineer's email?",
+  },
+  {
+    type: "input",
+    name: "github",
+    message: "What is the new Team Engineer's GitHub Profile name?",
+  },
+];
+// Intern Questions array
+const internQuestions = [
+  {
+    type: "input",
+    name: "name",
+    message: "What is the new Team Intern's name?",
+  },
+  {
+    type: "input",
+    name: "id",
+    message: "What is the new Team Intern's employee ID number?",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is the new Team Intern's email?",
+  },
+  {
+    type: "input",
+    name: "school",
+    message: "What school is the new Team Intern from?",
+  },
+];
+
+// inquire to finish building team
+const finishTeambuild = [
+  {
+    type: "list",
+    name: "add",
+    message: "Would you like to add an Engineer or Intern?",
+    choices: ["Engineer", "Intern", "done"],
+  },
+];
+
+// create a function to write the HTML file
+function writeToFile(fileName, data) {
+  return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+}
+
+// // create a function to end the questions menu and move to writing the html file
+// function questionsEnd() {
+
+// }
+function menuQuestions() {
+  inquirer.prompt(finishTeambuild).then((answer) => {
+    console.log(answer);
+    if (answer.add === "Engineer") {
+      inquirer.prompt(engineerQuestions).then((answer) => {
+        var newEngineer = new Engineer(
+          answer.name,
+          answer.id,
+          answer.email,
+          answer.github
+        );
+        teamList.push(newEngineer);
+        console.log(teamList);
+        menuQuestions();
+      });
+    } else if (answer.add === "Intern") {
+      inquirer.prompt(internQuestions).then((answer) => {
+        var newIntern = new Intern(
+          answer.name,
+          answer.id,
+          answer.email,
+          answer.school
+        );
+        teamList.push(newIntern);
+        console.log(teamList);
+        menuQuestions();
+      });
+    } else if (answer.add === "done") {
+      writeToFile("./dist/index.html", generateHTML(teamList));
+      console.log("new index.html created in dist directory.");
+    }
+  });
+}
+// create intialize function
+function init() {
+  inquirer.prompt(managerQuestions).then((answer) => {
+    var managerOne = new Manager(
+      answer.name,
+      answer.id,
+      answer.email,
+      answer.officeNumber
+    );
+    teamList.push(managerOne);
+    console.log(teamList);
+    menuQuestions();
+  });
+  // writeToFile('./dist/index.html', generateHTML(answer))
+}
+init();
 
 
 
 
-
-
-
-
-
-// GIVEN a command-line application that accepts user input
-// WHEN I am prompted for my team members and their information
-// THEN an HTML file is generated that displays a nicely formatted team roster based on user input
-// WHEN I click on an email address in the HTML
-// THEN my default email program opens and populates the TO field of the email with the address
-// WHEN I click on the GitHub username
-// THEN that GitHub profile opens in a new tab
-// WHEN I start the application
-// THEN I am prompted to enter the team manager’s name, employee ID, email address, and office number
-// WHEN I enter the team manager’s name, employee ID, email address, and office number
-// THEN I am presented with a menu with the option to add an engineer or an intern or to finish building my team
-// WHEN I select the engineer option
-// THEN I am prompted to enter the engineer’s name, ID, email, and GitHub username, and I am taken back to the menu
-// WHEN I select the intern option
-// THEN I am prompted to enter the intern’s name, ID, email, and school, and I am taken back to the menu
-// WHEN I decide to finish building my team
-// THEN I exit the application, and the HTML is generated
---
+++ src/generateHTML.js
@@ -0,0 +1,60 @@
+function generateCard(answer) {
+    var p = "";
+    if (answer.getRole() === "Manager") {
+      p = `    <p class="card-text my-0 p-2 bg-white border">Office Number: ${answer.officeNumber}</p>
+      `;
+    } else if (answer.getRole() === "Engineer") {
+      p = `     <p class="card-text my-0 p-2 bg-white border"><a href="https://github.com/${answer.github}" target="_blank">GitHub: ${answer.github}</a></p>
+      `;
+    } else {
+      p = `    <p class="card-text my-0 p-2 bg-white border">School: ${answer.school}</p>
+      `;
+    }
+    return `
+    <div class="shadow-lg card" style="width: 18rem;">
+      <div class="card-body bg-primary text-white">
+        <h5 class="card-title">${answer.name}</h5>
+        <h5 class="card-role">${answer.getRole()}</h5>
+      </div>
+      <div class="p-4 bg-light">
+        <p class="card-text my-0 p-2 bg-white border">ID: ${answer.id}</p>
+        <a href="mailto:${
+          answer.email
+        } class="card-text my-0 p-2 bg-white border">Email: ${answer.email}</a>
+        <div>${p}</div>
+      </div>
+    </div>
+  `;
+  }
+  
+  function generateHTML(data) {
+    var card = "";
+    for (var i = 0; i < data.length; i++) {
+      card += generateCard(data[i]);
+    }
+    return `
+      
+      <!DOCTYPE html>
+  <html lang="en">
+  <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+      <title>My Team</title>
+  </head>
+  <body>
+  <header>
+      <h1 class= "py-4 bg-danger text-white text-center font-weight-bolder"> My Team</h1> 
+  </header>
+  <main class="d-flex flex-wrap justify-content-around">
+  ${card}
+  </main>
+      
+  </body>
+  </html>
+      
+      `;
+  }
+  
+  module.exports = { generateHTML };
